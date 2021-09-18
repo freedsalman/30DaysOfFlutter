658bdf925646fbbb5c22bf76dd7ba45864f5629c
freed salman <freedsalmans855.fs@gmail.com>
2021-09-18 15:12:56 +0500

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('flutter app'),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("catalog app"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('flutter app'),
+        ),
+      ),
+    );
+  }
+}
