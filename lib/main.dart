3b80edd3c4940fbeeea816cb32e7070b95d835f5
freed salman <freedsalmans855.fs@gmail.com>
2021-09-21 19:02:30 +0500

    day 27 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/screens/cart_page.dart';
-import 'package:flutter_catalog/screens/home_page.dart';
 import 'package:flutter_catalog/screens/login_page.dart';
 import 'package:flutter_catalog/utilis/routes.dart';
-import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'screens/home_page.dart';
+import 'widgets/themes.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(VxState(store: MyStore(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.system,
       theme: MyTheme.lightTheme(context),
-      darkTheme: ThemeData(
-        brightness: Brightness.light,
-      ),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      initialRoute: MyRoutes.loginRoute,
+      initialRoute: MyRoutes.homeRoute,
       routes: {
+        "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
         MyRoutes.loginRoute: (context) => LoginPage(),
         MyRoutes.cartRoute: (context) => CartPage(),
--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -1,45 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class AddToCart extends StatefulWidget {
+class AddToCart extends StatelessWidget {
   final Item catalog;
-  const AddToCart({
+  AddToCart({
     Key key,
     this.catalog,
   }) : super(key: key);
 
   @override
-  _AddToCartState createState() => _AddToCartState();
-}
-
-class _AddToCartState extends State<AddToCart> {
-  final _cart = CartModel();
-  @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(widget.catalog) ?? false;
-
+    VxState.listen(context, to: [AddMutation]);
+    final CartModel _cart = (VxState.store as MyStore).cart;
+    bool isInCart = _cart.items.contains(catalog) ?? false;
     return RaisedButton(
-      color: Colors.black87,
-      child: Row(
-        children: [
-          isInCart ? Icon(Icons.done) : "Add to cart".text.make(),
-          Text(
-            "",
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-        ],
-      ),
-      onPressed: () {
-        isInCart = isInCart.toggle();
-        final _catalog = CatalogModel();
-        final _cart = CartModel();
-        _cart.catalog = _catalog;
-        _cart.add(widget.catalog);
-        setState(() {});
-      },
-    );
+        color: Colors.black87,
+        child: Row(
+          children: [
+            isInCart ? Icon(Icons.done) : "Add to cart".text.make(),
+            Text(
+              "",
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          ],
+        ),
+        onPressed: () {
+          if (!isInCart) {
+            AddMutation(catalog);
+          }
+        });
   }
 }
